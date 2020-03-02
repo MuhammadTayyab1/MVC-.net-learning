9cc30d3aac52423cd4ac0e9efdb8140e44ca2715
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2020-03-02 10:10:25 -0800

    student project

---
+++ Student project/studentproject/Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace studentproject.Controllers
+{
+    public class StudentController : Controller
+    {
+        //
+        // GET: /Student/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult addstudent()
+        {
+            return View();
+        }
+
+        public ActionResult viewstudent()
+        {
+            return View();
+        }
+
+        public ActionResult updatestudent()
+        {
+            return View();
+        }
+
+        public ActionResult deletestudent()
+        {
+            return View();
+        }
+	}
+
+}
